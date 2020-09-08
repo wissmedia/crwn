67df665ef70c66b6879c7719dcf3740f2aed82b2
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-09-08 21:49:22 +0800

    add some change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HomePage } from './HomePage.jsx'
+import { HomePage } from './pages/homepage/HomePage.jsx'
 import './App.scss';
 
 function App() {
---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Directory from '../../components/directory/Directory'
+import './HomePage.scss'
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
