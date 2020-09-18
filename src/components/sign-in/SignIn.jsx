773ff735099b16756474a1f1989fe96fa32f0c10
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-09-18 22:35:01 +0800

    edit button

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -43,7 +43,7 @@ class SignIn extends React.Component {
 
           <div className='buttons'>
             <CustomButton type="submit">SIGN IN</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn >SIGN IN WITH GOOGLE</CustomButton>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn >SIGN IN WITH GOOGLE</CustomButton>
           </div>
         </form>
       </div>
